e5c376a8b9cd59e51c9e89e3678e406b99d5835f
Александр Нашиван <alex@moonion.com>
2017-09-01 16:42:40 +0300

    video

--- src/user/index.js
+++ src/user/index.js
@@ -18,7 +18,7 @@ import ChangePassword from './containers/ChangePassword';
 
 const routes = [
     <DefaultLayout key="Sign" path="/sign/:type/:user?" component={Sign} hideHeader/>,
-    <PrivateRoute key="ChangePassword" path="/settings/password" component={ChangePassword}/>,
+    <PrivateRoute key="ChangePassword" path="/settings/password" component={ChangePassword} hideBackgroundTopMobileHeader/>,
     <PrivateRoute key="Settings" path="/settings" component={Settings} hideBackgroundTopMobileHeader/>,
     <PrivateRoute key="Edit" path="/profile/edit" component={Edit} hideBackgroundTopMobileHeader/>,
     <PrivateRoute key="Delete" path="/profile/delete" component={Delete}/>,
