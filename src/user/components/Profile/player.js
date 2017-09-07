2cb4dcac7a5f74b97fc4db50256134a0e2f51e4f
Александр Нашиван <alex@moonion.com>
2017-09-07 16:19:55 +0300

    other team

--- src/user/components/Profile/player.js
+++ src/user/components/Profile/player.js
@@ -687,7 +687,8 @@ class PlayerProfile extends Component {
                                             className={classes.infoCardName}>{user.first_name} {user.last_name}</Typography>
                             </Hidden>
                             <Typography type="subheading" align="center" className={classes.infoCardTeam}>
-                                {user.team || 'Team Unknown'}{user.league_short ? ' - ' + user.league_short : ''}
+                                {!!!user.team_status && 'Other / '}{user.team || 'Team Unknown'}
+                                {!!!user.league_status && ' - Other / '}{user.league_short ? ' - ' + user.league_short : (!!!user.league_status ? user.league : '')}
                             </Typography>
                             <Typography type="body1" align="center">
                                 {(user.team_location !== 'n/a' && user.team_location) || user.team_country || 'Location Unknown'}
