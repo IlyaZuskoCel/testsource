4457d326d2258410b5002d2105ff5af134ac6f96
vitaly <petrushkov1997@mail.ru>
2018-05-10 23:35:41 +0300

    addServerTemplateChangeBabelDockerDockerLockNginxPackageYarn

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ WORKDIR /usr/src
 
 RUN yarn install
 
+ENV NODE_ENV=production
+ENV NODE_ENV=development
+
 RUN yarn run build
 
 WORKDIR /
@@ -18,5 +21,7 @@ RUN chmod +x run.sh
 
 
 VOLUME ["/var/www/app", "/etc/nginx/conf.d/app"]
+EXPOSE 80
 
-CMD ["./run.sh"]
+#CMD ["./run.sh"]
+CMD ["yarn", "run", "start:server"]
---
+++ src/server.js
@@ -0,0 +1,69 @@
+import path from "path";//
+
+import {renderToString} from "react-dom/server";
+import React from 'react'
+import {StaticRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createExpressServer} from "create-react-server";
+import Cookie from 'react-cookies';
+import {SheetsRegistry} from 'react-jss/lib/jss';
+import JssProvider from 'react-jss/lib/JssProvider';
+import {create} from 'jss';
+import preset from 'jss-preset-default';
+import createGenerateClassName from 'material-ui/styles/createGenerateClassName';
+
+
+import config from "../webpack.config";
+import app from "./app";
+import configureStore from "./store";
+
+import theme from "./theme";
+import template from "./template";
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const options = {
+    render: (req, res, initialProps, context) => {
+        const sheetsRegistry = new SheetsRegistry();
+        const jss = create(preset());
+        jss.options.createGenerateClassName = createGenerateClassName;
+        const Theme = theme(new Map());
+
+        Cookie.setRawCookie(req.headers.cookie);
+        const state = context.store ? context.store.getState() : {};
+        const store = configureStore(state);
+        const html = renderToString(React.createElement(
+            JssProvider,
+            {registry: sheetsRegistry, jss: jss},
+            React.createElement(
+                Provider,
+                {store: store},
+                React.createElement(
+                    StaticRouter,
+                    {location: req.url, context: context},
+                    React.createElement(
+                        Theme,
+                        {},
+                        options.app({
+                            props: initialProps,
+                            req: req,
+                            res: res,
+                            state: state
+                        })
+                    )
+                )
+            )
+        ));
+        const css = sheetsRegistry.toString();
+        return {html, css}
+    },
+    app,
+    template,
+    templatePath: path.join(config.output.path, 'index.html'),
+    outputPath: config.output.path,
+    port: process.env.PORT || 80,
+    debug: NODE_ENV === 'development'
+};
+
+
+createExpressServer(options);
