016e90eb7e91bfeb9563d9441ab37b5579444fd6
anton.ivanov <anton.ivanov@moonion.com>
2018-05-21 12:59:38 +0300

    edit PlayerCard for step 4

--- src/video/components/VideoItem/index.js
+++ src/video/components/VideoItem/index.js
@@ -207,6 +207,7 @@ const styleSheet = createStyleSheet('VideoItem', theme => ({
         width: '100%',
         cursor: 'pointer',
         maxHeight: 600,
+        fitObject: 'contain',
         [theme.breakpoints.down('md')]: {
             maxHeight: 450,
         },
